78bcc2d3ea7463497a567c3aaee939ecc268ec85
asharma72 <ag.sharma23@gmail.com>
2026-03-22 22:15:24 +0530

    feat: add Services/IEcommerceService.cs

---
+++ Services/IEcommerceService.cs
@@ -0,0 +1,11 @@
+namespace Ecommerce.API.Services
+{
+    public interface IEcommerceService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
