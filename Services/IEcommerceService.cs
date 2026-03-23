17180e0a9a0422c8f64c4c0f294ca4095f1bbfe5
asharma72 <ag.sharma23@gmail.com>
2026-03-23 10:21:40 +0530

    feat: add Services/IEcommerceService.cs

--- Services/IEcommerceService.cs
+++ Services/IEcommerceService.cs
@@ -8,4 +8,37 @@ namespace Ecommerce.API.Services
         Task UpdateAsync(T entity);
         Task DeleteAsync(int id);
     }
+
+    public interface IProductService : IEcommerceService<Product>
+    {
+    }
+
+    public interface IOrderService : IEcommerceService<Order>
+    {
+    }
+
+    public interface ICustomerService : IEcommerceService<Customer>
+    {
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public DateTime Date { get; set; }
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
 }
