6d557df7a06aaf08a6fb3d64fcc139e8c10cb06a
asharma72 <ag.sharma23@gmail.com>
2026-03-23 09:48:32 +0530

    feat: add Services/IEcommerceService.cs

--- Services/IEcommerceService.cs
+++ Services/IEcommerceService.cs
@@ -1,11 +1,52 @@
 namespace Ecommerce.API.Services
 {
-    public interface IEcommerceService<T> where T : class
+    public interface IProductService
     {
-        Task<T> GetByIdAsync(int id);
-        Task<IEnumerable<T>> GetAllAsync();
-        Task<T> CreateAsync(T entity);
-        Task UpdateAsync(T entity);
-        Task DeleteAsync(int id);
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> GetProductByIdAsync(int id);
+        Task CreateProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+    }
+
+    public interface IOrderService
+    {
+        Task<IEnumerable<Order>> GetAllOrdersAsync();
+        Task<Order> GetOrderByIdAsync(int id);
+        Task CreateOrderAsync(Order order);
+        Task UpdateOrderAsync(Order order);
+        Task DeleteOrderAsync(int id);
+    }
+
+    public interface ICustomerService
+    {
+        Task<IEnumerable<Customer>> GetAllCustomersAsync();
+        Task<Customer> GetCustomerByIdAsync(int id);
+        Task CreateCustomerAsync(Customer customer);
+        Task UpdateCustomerAsync(Customer customer);
+        Task DeleteCustomerAsync(int id);
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Total { get; set; }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
