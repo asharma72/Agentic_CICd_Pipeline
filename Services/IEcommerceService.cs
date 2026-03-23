ba641c3db47c69c96e9ae0f7b49c5e15a524ad5c
asharma72 <ag.sharma23@gmail.com>
2026-03-23 09:17:44 +0530

    feat: add Services/IEcommerceService.cs

--- Services/IEcommerceService.cs
+++ Services/IEcommerceService.cs
@@ -33,12 +33,14 @@ namespace Ecommerce.API.Services
         public int Id { get; set; }
         public int CustomerId { get; set; }
         public DateTime OrderDate { get; set; }
+        public decimal Total { get; set; }
     }
 
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
     }
 }
