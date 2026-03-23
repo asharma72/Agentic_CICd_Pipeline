a0a6dbff9acfd415bb785c2c671eb0d70db3f129
asharma72 <ag.sharma23@gmail.com>
2026-03-23 10:48:25 +0530

    feat: add Services/IEcommerceService.cs

--- Services/IEcommerceService.cs
+++ Services/IEcommerceService.cs
@@ -20,25 +20,25 @@ namespace Ecommerce.API.Services
     public interface ICustomerService : IEcommerceService<Customer>
     {
     }
-}
 
-public class Product
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-}
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
 
-public class Order
-{
-    public int Id { get; set; }
-    public int CustomerId { get; set; }
-    public DateTime Date { get; set; }
-}
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
 
-public class Customer
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
 }
